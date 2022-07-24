d540feeb6c3fa40655063304e3642d4e30d3b945
UlinEgor <62210255+UlinEgor@users.noreply.github.com>
2022-07-24 11:06:42 +0300

    nado

--- monitor.py
+++ monitor.py
@@ -1,4 +1,7 @@
-def paintOnMonitor(sc, x1, y1, l, h, x2, y2, WIN_WIDTH, pr, ORANGE, r, pygame, x, y):
+WHITE = (255, 255, 255)
+ORANGE = (255, 150, 100)
+
+def paintOnMonitor(sc, x1, y1, l, h, x2, y2, WIN_WIDTH, pr, r, pygame, x, y):
     sc.fill((0,0,0))
     pygame.draw.rect(sc, (255, 255, 255), 
                      (x1, y1, l, h)) 
